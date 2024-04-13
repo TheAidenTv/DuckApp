4a41ac42797d2418118a6ab5b8b25f9bf8a4d717
Aiden Timmons <aidentimmons@hotmail.com>
2024-04-13 08:56:14 -0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace DuckApp;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+        GenerateRandomDuck(this, EventArgs.Empty);
+    }
+
+    private async void GenerateRandomDuck(object sender, EventArgs e)
+    {
+        string url = "https://random-d.uk/api/random";
+
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string json = await response.Content.ReadAsStringAsync();
+            Duck duck = JsonConvert.DeserializeObject<Duck>(json);
+
+            duckImage.Source = duck.url;
+        }
+        else
+        {
+            duckImage.Source = "error.png";
+        }
+    }
+}
+
+internal class Duck
+{
+    public string url { get; set; }
+}
